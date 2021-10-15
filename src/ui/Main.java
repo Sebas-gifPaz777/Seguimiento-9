469df29656adeb200e1e2c6212c0c26879cbd418
Sebas-gifPaz777 <pazsebas797@gmail.com>
2021-10-15 13:28:19 -0500

    First commit

---
+++ src/ui/Main.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		boolean exit=false;
+		
+		while(!exit) {
+			Scanner sn=
+			System.out.println("Choose an option");
+			
+		}
+	}
+}
